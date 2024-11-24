22acb027c90515f37161dc044372b7279bd70c7e
Biromedic <birkancemil2015@gmail.com>
2024-11-24 20:25:37 +0300

    Add Controllers

--- src/main/java/com/example/airlineapi/controller/UserController.java
+++ src/main/java/com/example/airlineapi/controller/UserController.java
@@ -2,6 +2,9 @@ package com.example.airlineapi.controller;
 
 import com.example.airlineapi.model.User;
 import com.example.airlineapi.service.serviceInterface.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -10,13 +13,16 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Tag(name = "User API", description = "Operations related to user management")
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/{userId}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<User> getUserById(@PathVariable Long userId) {
+    @Operation(summary = "Get user by ID", description = "Retrieves a user by their unique ID. Accessible only to admins.")
+    public ResponseEntity<User> getUserById(
+            @PathVariable @Parameter(description = "ID of the user to retrieve", required = true) Long userId) {
         User user = userService.getUserById(userId);
         return ResponseEntity.ok(user);
     }
